96ec1fbc6cc44ba09c6decaf838dfc6a7593064c
Oleksandra <ozvoryhina@kse.org.ua>
2022-12-23 14:45:42 +0200

    Zvoryhina task1

--- assg7.py
+++ assg7.py
@@ -1,7 +1,30 @@
 import argparse
 import sys
 parser = argparse.ArgumentParser(description="our example parser.")
+parser.add_argument("--filename", "-f", required=True)
+parser.add_argument('--output', required=False)
+parser.add_argument("--medals", action="store_true", required=False)
+parser.add_argument('--country', required=False)
+parser.add_argument('--year', required=False)
+
 args = parser.parse_args()
+
+
+if args.medals:
+    output_file = None
+    row = 0
+    if args.output is not None:
+        output_file = open(args.output, "w")
+    with open(args.filename, "r") as file:
+        for line in file:
+            data = line.strip().split("\t")
+            if data[7] == args.country and data[9] == args.year:
+                if row < 10:
+                    row += 1
+                    out_line = f"{data[1]}, {data[-2]}, {data[-1]}\n"
+                    print(out_line, end="")
+                    if output_file is not None:
+                        output_file.write(out_line)
 
 
 
@@ -84,4 +107,12 @@ if args.overall:
     
 if args.interactive:
     interactive()
+print(args)
+
+
+print(f"{args.filename=}")
+print(f"{args.medals=}")
+print(f"{args.year=}")
+print(f"{args.country=}")
+
 print(args)
